98cac84509b039d7940cc511bd5ae3c88ab1ce52
kr4k3nb1t3 <95190880+kr4k3nb1t3@users.noreply.github.com>
2021-12-01 16:03:24 -0500

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && apt-get install xorg fluxbox -y
 sudo cp -r fluxbox /usr/share/fluxbox
 sudo rm -r /etc/X11/fluxbox
-sudo cp -r fluxbox /etc/X11/fluxbox
+sudo cp -r fluxbox /etc/X11/
 sudo mkdir ~/.fluxbox
 sudo mv ~/.fluxbox /root
 sudo cp -r fluxbox ~/.fluxbox 
