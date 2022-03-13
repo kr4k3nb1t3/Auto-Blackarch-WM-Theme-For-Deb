306cbc81af72367c83b282941f99099b2fac6693
kr4k3nb1t3 <95190880+kr4k3nb1t3@users.noreply.github.com>
2022-03-13 04:34:12 -0500

    Update autoconf.sh

--- autoconf.sh
+++ autoconf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 363A96A5CEA9EA27
-sudo apt-get update && sudo apt-get install xorg fluxbox rxvt-unicode -y --fix-missing
+wget https://deb.parrot.sh/parrot/pool/main/p/parrot-archive-keyring/parrot-archive-keyring_2021.7%2bparrot1_all.deb
+dpkg -i parrot-archive-keyring_2021.7+parrot1_all.deb
+sudo apt update && sudo apt install xorg fluxbox rxvt-unicode -y --fix-missing
 sudo rm -r /usr/share/fluxbox
 sudo cp -r fluxbox /usr/share/fluxbox
 sudo rm -r /etc/X11/fluxbox
